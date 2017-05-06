b72b7f00355951118eba71b3966b30c0344325fb
MichaelPanW <m28126584@gmail.com>
2017-05-07 00:44:19 +0800

    amp調整

--- Dcard/Trade/Controller/IndexController.class.php
+++ Dcard/Trade/Controller/IndexController.class.php
@@ -63,6 +63,7 @@ class IndexController extends GlobalController {
 			$article['url']=utf8_decode($article['url']);
 			$article['content']=utf8_decode($article['content']);
 			$article['content']=str_replace("img", "amp-img", $article['content']);
+			$article['content']=str_replace("style=", "class=", $article['content']);
 			$amp='
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>';
 		$this->assign('amp',$amp);
